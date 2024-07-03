da3e3aad0bc4bf98f3389856c6e9c3ebb5266ae0
florea1337 <97162131+florea1337@users.noreply.github.com>
2024-07-03 21:23:22 +0300

    Add files via upload

---
+++ Gym/Gym/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+//this calc program not a %100.But you can use this program for base.The program calculates your daily intake
+// of fat, protein, carbohydrates and maximum calories 
+int main()
+{
+    float weight{};
+    float weightgoal{};
+    float nrmlcalorie;
+    float caloriefordiet;
+    float proteincalcgr;
+    float proteincalckj;
+    float fatcalckj;
+    float fatcalcgr;
+    float carbcalckj;
+    float carbcalcgr;
+
+
+	cout << "Please Write Your Weight: ";
+	cin >> weight;
+	
+	cout << "Please Enter Your Weight Goal: ";
+	cin >> weightgoal;
+
+    nrmlcalorie = weight * 33;
+    caloriefordiet = nrmlcalorie - 400;
+    proteincalcgr = weightgoal * 2.2;
+    proteincalckj = proteincalcgr * 4;
+    fatcalckj = caloriefordiet * 0.39;
+    fatcalcgr = fatcalckj / 9;
+    carbcalckj = caloriefordiet - (proteincalckj + fatcalckj);
+    carbcalcgr = carbcalckj / 4;
+
+	cout << "Your Maximum Number of Calories You Should Consume Per Day: \n" << caloriefordiet << endl;
+    cout << "Your Maximum Number of Protein(gr) You Should Consume Per Day: \n" << proteincalcgr << endl;
+    cout << "Your Maximum Number of Protein(kj) You Should Consume Per Day: \n" << proteincalckj << endl;
+    cout << "Your Maximum Number of Fat You(gr) Should Consume Per Day: \n" << fatcalckj << endl;
+    cout << "Your Maximum Number of Fat(kj) You Should Consume Per Day: \n" << fatcalcgr << endl;
+    cout << "Your Maximum Number of Carbohydrate(gr) You Should Consume Per Day: \n" << carbcalckj << endl;
+    cout << "Your Maximum Number of Carbohydrate(kj) You Should Consume Per Day: \n" << carbcalcgr << endl;
+
+    //cout << caloriefordiet << proteincalcgr << proteincalckj << fatcalckj << fatcalcgr << carbcalckj << carbcalcgr;
+
+}
